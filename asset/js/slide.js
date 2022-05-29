ccb520e78adc7a03172cfe485ca6e20841da263f
CyberAli1989 <naserianali@gmail.com>
2022-05-30 01:26:53 +0430

    add slide to menu 1

---
+++ asset/js/slide.js
@@ -0,0 +1,35 @@
+$(function() {
+    let slide = false
+    let head = false
+    let abotu = false
+    $("#home").click(function(e) {
+        if (head == true) {
+            $('header').slideToggle();
+            
+        }
+        $('body').css('overflow', 'hidden');
+
+    })
+    $("#about").click(function (e) { 
+        e.preventDefault();
+        $('header').slideUp();
+        head = true
+        if (about == true) {
+            $('#About').slideToggle();
+
+        }
+        $('body').css('overflow', 'visible');
+        $('#Contact').css('display', 'none');
+    });
+    $("#contact").click(function (e) { 
+        e.preventDefault();
+        $('#About').slideUp();
+        $('header').slideUp();
+        about = true
+        head = true
+        $('body').css('overflow', 'hidden');
+        $('#Contact').css('display', 'initial');
+
+    });
+    
+})

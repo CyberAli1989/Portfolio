509db27748bc79bd69b9dc7d3cba8cb2025bdbbc
CyberAli1989 <naserianali@gmail.com>
2022-05-31 00:11:28 +0430

    imporove menu on responsive

--- asset/js/chngeTheme.js
+++ asset/js/chngeTheme.js
@@ -13,18 +13,19 @@ $(function () {
             $('body').css('background', '#fff');
             $('#headDesc p').css('color', '#000');
             $('#headDesc .btn button.learn-more .button-text').css('color', '#000');
-            $('#headDesc .title h2').css('color', '#72b626');
+            $('#headDesc .title h2').css('color', '#72b626' );
             $('#headDesc .btn button.learn-more .circle').css('background', '#72b626');
             $('#headDesc .btn button.learn-more .button-text').css('border', '1px solid #72b626');
             $('#nav ul li:before').css('background', '#72b626');
             styleElem.innerHTML = "#nav ul li:before {background: #72b626 !important;}"
+            $('#nav ul').css('background', '#ffffff')
             $('#nav ul li').hover(function () {
                 // over
                 this.style.background = '#72b626';
 
             }, function () {
-                // out
                 this.style.background = '#eeeeee';
+                // out
 
             });
             // darkMod = false;
@@ -39,15 +40,17 @@ $(function () {
             $('#headDesc .btn button.learn-more .button-text').css('color', '#fff');
             $('#headDesc .btn button.learn-more .button-text').css('border', '1px solid #ffb400');
             styleElem.innerHTML = "#nav ul li:before {background: #ffb400 !important;}";
-            $('#headDesc .title h2').css('color', '#ffb400')
-            $('#headDesc .btn button.learn-more .circle').css('background', '#ffb400')
+            $('#headDesc .title h2').css('color', '#ffb400');
+            $('#nav ul').css('background', '#2b2a2a')
+            $('#nav ul li').css('background', '#444444')
+            $('#headDesc .btn button.learn-more .circle').css('background', '#ffb400');
             $('#nav ul li').hover(function () {
                 // over
                 this.style.background = '#ffb400';
 
             }, function () {
                 // out
-                this.style.background = '#2b2a2a';
+                this.style.background = '#444444';
 
             });
 
